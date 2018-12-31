c0cc6572f39c8100536db1b9f263dfc5f0afe6f8
Julian Finkler <julian@mintware.de>
2018-12-31 23:38:43 +0100

    Copyright updated

--- src/ComposeExtension.php
+++ src/ComposeExtension.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Twig Compose package.
  *
- * Copyright 2018 by Julian Finkler <julian@developer-heaven.de>
+ * Copyright 2018-now by Julian Finkler <julian@developer-heaven.de>
  *
  * For the full copyright and license information, please read the LICENSE
  * file that was distributed with this source code.
